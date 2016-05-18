bef572f8206d476366a57dc94bd42a985f2de532
Leandro Parazito <leandroh@gmail.com>
2016-05-18 14:28:43 -0300

    Add autoscroll to live event video

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,19 @@ $(window).scroll(function(){
 
 $(window).load(function(){
 	handleTopNavAnimation();
+
+	var liveSection = document.querySelector('a[href="#live"');
+	liveSection.click();
 });
 
 function handleTopNavAnimation() {
 	var top=$(window).scrollTop();
 
 	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
+		$('#site-nav').addClass('navbar-solid');
 	}
 	else{
-		$('#site-nav').removeClass('navbar-solid'); 
+		$('#site-nav').removeClass('navbar-solid');
 	}
 }
 
@@ -27,7 +30,7 @@ function handleTopNavAnimation() {
 
 $('#registration-form').submit(function(e){
     e.preventDefault();
-    
+
     var postForm = { //Fetch form data
             'fname'     : $('#registration-form #fname').val(),
             'lname'     : $('#registration-form #lname').val(),
